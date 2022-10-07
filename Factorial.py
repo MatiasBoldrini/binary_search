fa4f4f43214d02214635d4c9650d1b3945aa902d
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 <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>
2022-10-07 16:57:24 -0300

    msg

---
+++ Factorial.py
@@ -0,0 +1,24 @@
+import unittest
+import random
+
+class ElementNotFoundException(Exception):
+    pass
+class InvalidInputException(Exception):
+    pass
+
+def search(list,element, index=0): 
+    if len(list) < 1 and element is not None:
+        raise InvalidInputException()
+    # divide list in two
+    list_divider = len(list)//2
+    if element != list[0] and len(list) == 1: 
+        raise ElementNotFoundException()
+    if element == list[0]:
+        return index
+    if element >= list[list_divider]:
+        right_list = list[list_divider:]
+        return search(right_list, element, list_divider+index)
+    if element < list[list_divider]:
+        left_list = list[:list_divider]
+        return search(left_list,element, index)
+
---
+++ test_factorial.py
@@ -0,0 +1,42 @@
+from Factorial import search, ElementNotFoundException, InvalidInputException
+import unittest, random
+class Test(unittest.TestCase):
+    def testCase_0(self):
+        with self.assertRaises(ElementNotFoundException):
+            lista = [0]
+            result = search(lista, 2)
+
+    def testCase_1(self):
+        lista = [2]
+        result = search(lista, 2)
+        self.assertEqual(result, 0)
+
+    def testCase_2(self):
+        lista = [2, 4, 6, 8]
+        result = search(lista, 4)
+        self.assertEqual(result, 1)
+
+    def testCase_3(self):
+        lista = [1, 3, 5, 7, 9, 11, 12, 13, 14, 15, 16, 17, 18]
+
+        result = search(lista, 15)
+        self.assertEqual(result, 9)
+
+    def testCase_4(self):
+        lista = [1, 3, 5, 7, 9]
+        result = search(lista, 9)
+        self.assertEqual(result, 4)
+
+    def testCase_4(self):
+        with self.assertRaises(ElementNotFoundException):
+            lista = [1, 3, 5, 7, 9]
+            result = search(lista, 50)
+    
+    def testCase_6(self):
+        with self.assertRaises(InvalidInputException):
+            lista = []
+            result = search(lista, 2)
+            
+
+if __name__ == "__main__":
+    unittest.main()
