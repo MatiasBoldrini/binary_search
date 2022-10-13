721e9b632e25ba49433c27c52ad19c8ed53bd0c3
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 <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>
2022-10-12 22:04:01 -0300

    Better documentation

--- Factorial.py
+++ Factorial.py
@@ -4,9 +4,20 @@ class InvalidInputException(Exception):
     pass
 
 def search(list,element, index=0): 
+    """
+    The function takes an ordered list, an element to search for, and an index. It then divides the list in two,
+    and checks if the element is in the left or right half of the list. If it's in the left half, it
+    recursively calls itself on the left half of the list, and if it's in the right half, it recursively
+    calls itself on the right half of the list. If the element is not in the list, it raises an
+    exception.
+    
+    :param list: the list to search through
+    :param element: the element to search for
+    :param index: the index of the element in the original list, defaults to 0 (optional)
+    :return: The index of the element in the list
+    """
     if len(list) < 1 and element is not None:
         raise InvalidInputException()
-    # divide list in two
     list_divider = len(list)//2
     if element != list[0] and len(list) == 1: 
         raise ElementNotFoundException()
